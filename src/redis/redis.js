032154fbdbc1ba86f6ec94d2709adba237b5ae3c
duonghb53 <duonghb53@gmail.com>
2024-12-25 13:46:14 +0700

    add dotenv config

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,4 +1,6 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
 export const DEFAULT_TTL = 86400; // 1 day in seconds
